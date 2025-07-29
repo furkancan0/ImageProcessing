580175d96c878120a98f0dfd71ceadda0c99cd01
Furkan <cn_furkan@hotmail.com>
2025-07-29 22:47:33 +0300

    imageService test added

--- src/test/java/com/ImageProcessing/service/ImageServiceTest.java
+++ src/test/java/com/ImageProcessing/service/ImageServiceTest.java
@@ -1,11 +1,12 @@
 package com.ImageProcessing.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImageServiceTest {
-
+    MockMultipartFile firstFile = new MockMultipartFile("image", "image.txt", "text/plain", "some xml".getBytes());
     @Test
     void uploadImage() {
     }
