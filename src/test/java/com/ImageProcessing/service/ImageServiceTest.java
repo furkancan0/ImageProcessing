a539b65960dc8bb37cbfb7e9442b328b8aa3d861
Furkan <cn_furkan@hotmail.com>
2025-07-29 22:18:14 +0300

    first commit

---
+++ src/test/java/com/ImageProcessing/service/ImageServiceTest.java
@@ -0,0 +1,12 @@
+package com.ImageProcessing.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageServiceTest {
+
+    @Test
+    void uploadImage() {
+    }
+}
